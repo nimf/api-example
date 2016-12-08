b7e781252fd6100f5cdc94ad45cd08414bc1ff01
Yuriy Golobokov <yuriy.golobokov@icloud.com>
2016-12-08 16:47:30 +0600

    URL path versioning for contacts

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -6,31 +6,31 @@ class ContactsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get contacts_url, as: :json
+    get v1_contacts_url, as: :json
     assert_response :success
   end
 
   test "should create contact" do
     assert_difference('Contact.count') do
-      post contacts_url, params: { contact: { first_name: @contact.first_name, last_name: @contact.last_name, phone: @contact.phone } }, as: :json
+      post v1_contacts_url, params: { contact: { first_name: @contact.first_name, last_name: @contact.last_name, phone: @contact.phone } }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show contact" do
-    get contact_url(@contact), as: :json
+    get v1_contact_url(@contact), as: :json
     assert_response :success
   end
 
   test "should update contact" do
-    patch contact_url(@contact), params: { contact: { first_name: @contact.first_name, last_name: @contact.last_name, phone: @contact.phone } }, as: :json
+    patch v1_contact_url(@contact), params: { contact: { first_name: @contact.first_name, last_name: @contact.last_name, phone: @contact.phone } }, as: :json
     assert_response 200
   end
 
   test "should destroy contact" do
     assert_difference('Contact.count', -1) do
-      delete contact_url(@contact), as: :json
+      delete v1_contact_url(@contact), as: :json
     end
 
     assert_response 204

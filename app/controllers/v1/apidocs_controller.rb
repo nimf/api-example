09ccde63830f6a1a19cd88639333d52247fb2bfb
Yuriy Golobokov <yuriy.golobokov@icloud.com>
2016-12-09 08:51:33 +0600

    Added swagger-blocks gem and v1 docs controller

---
+++ app/controllers/v1/apidocs_controller.rb
@@ -0,0 +1,44 @@
+module V1
+  class ApidocsController < ActionController::Base
+    include Swagger::Blocks
+
+    swagger_root do
+      key :swagger, '2.0'
+      info do
+        key :version, '1.0.0'
+        key :title, 'Phonebook API'
+        key :description, <<~HEREDOCS
+          You can use this API to store and read contacts information
+        HEREDOCS
+        key :termsOfService, 'http://localhost:3000/terms/'
+        contact do
+          key :name, 'OpenIT API Team'
+        end
+        license do
+          key :name, 'MIT'
+        end
+      end
+      tag do
+        key :name, 'contact'
+        key :description, 'Contacts operations'
+        externalDocs do
+          key :description, 'Find more info here'
+          key :url, 'https://swagger.io'
+        end
+      end
+      key :host, 'localhost:3000'
+      key :basePath, '/v1'
+      key :consumes, ['application/json']
+      key :produces, ['application/json']
+    end
+
+    # A list of all classes that have swagger_* declarations.
+    SWAGGERED_CLASSES = [
+      self
+    ].freeze
+
+    def index
+      render json: Swagger::Blocks.build_root_json(SWAGGERED_CLASSES)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :contacts
   end
+  scope :docs do
+    namespace :v1 do
+      resources :apidocs, only: :index
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
